7e73d6e6775ca28402b3008d06a659cef85179f1
wildenali <wildenwildenaliali@gmail.com>
2020-09-15 23:52:56 +0700

    css untuk .app__videos

--- src/Video.js
+++ src/Video.js
@@ -4,16 +4,9 @@ import "./Video.css";
 function Video() {
   return (
     <div className="video">
-      <video className="video__player" loop src=""></video>
-      <iframe
-        className="video__player"
-        width="1189"
-        height="669"
-        src="https://www.youtube.com/embed/QZG1CVhZ3RQ"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+      <img src="https://a.wattpad.com/cover/186956711-352-k211861.jpg"></img>
+
+      {/* <video className="video__player" loop src=""></video> */}
 
       {/* VideoFooter */}
 
