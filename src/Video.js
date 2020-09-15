46e011ab83ff4daf951e6e934097cec4d783ddfd
wildenali <wildenwildenaliali@gmail.com>
2020-09-16 00:04:03 +0700

    Ganti gambarnyadulu

--- src/Video.js
+++ src/Video.js
@@ -4,7 +4,7 @@ import "./Video.css";
 function Video() {
   return (
     <div className="video">
-      <img src="https://a.wattpad.com/cover/186956711-352-k211861.jpg"></img>
+      <img src="https://wallpaperaccess.com/full/140057.jpg"></img>
 
       {/* <video className="video__player" loop src=""></video> */}
 
