9fcaf8e96b60139719ea4b7cde4d2a4f9335ebbb
wildenali <wildenwildenaliali@gmail.com>
2020-09-15 22:50:22 +0700

    Create Video Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
+import Video from "./Video";
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
       <h1>Cek</h1>
+
+      {/* app container */}
+      {/* videos */}
+      <Video />
+      {/* <Videos /> */}
+      {/* <Videos /> */}
+      {/* <Videos /> */}
+      {/* <Videos /> */}
+      {/* <Videos /> */}
     </div>
   );
 }
---
+++ src/Video.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+      <video src="https://youtu.be/QZG1CVhZ3RQ"></video>
+
+      {/* VideoFooter */}
+
+      {/* VideoSIdebar */}
+    </div>
+  );
+}
+
+export default Video;
