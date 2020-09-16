558ad6605e4383e933649a6730b50698eb970bfe
wildenali <wildenwildenaliali@gmail.com>
2020-09-16 10:49:36 +0700

    Create VideoSidebar component

---
+++ src/VideoSidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./VideoSidebar.css";
+
+function VideoSidebar() {
+  return (
+    <div className="videoSidebar">
+      <h1>Ini Video Sidebar</h1>
+    </div>
+  );
+}
+
+export default VideoSidebar;
