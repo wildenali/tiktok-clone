9db24a408f8d9e4a5ab058803160f0046672fcc0
wildenali <wildenwildenaliali@gmail.com>
2020-09-16 10:40:28 +0700

    haldeVideoPress, so when the video clicked it will play and if click again it will paused

--- src/Video.js
+++ src/Video.js
@@ -1,22 +1,36 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./Video.css";
 
 function Video() {
+  const [playing, setPlaying] = useState(false);
   const videoRef = useRef(null);
+
+  const handleVideoPress = () => {
+    // if video is playing
+    // stop it...
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+
+    // otherwise if its not playing
+    // play it...
+  };
 
   return (
     <div className="video">
       <video
+        onClick={handleVideoPress}
         className="video__player"
         loop
         ref={videoRef}
         src="/Videos/contoh2.mp4"
         type="video/mp4"
-        controls
       ></video>
-
       {/* VideoFooter */}
-
       {/* VideoSIdebar */}
     </div>
   );
