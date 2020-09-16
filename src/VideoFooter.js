ab99988a8cb2a7bcb1aedcbc81b4cb90ac129ce0
wildenali <wildenwildenaliali@gmail.com>
2020-09-16 10:49:10 +0700

    Create VideoFooter Component

---
+++ src/VideoFooter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./VideoFooter.css";
+
+function VideoFooter() {
+  return (
+    <div className="videoFooter">
+      <h1>Ini Video Footer</h1>
+    </div>
+  );
+}
+
+export default VideoFooter;
