dad607cfbd5227736b7b797c136788b3cba8816d
wildenali <wildenwildenaliali@gmail.com>
2020-09-16 14:26:11 +0700

    Icon for sideBar

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./VideoSidebar.css";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MessageIcon from "@material-ui/icons/Message";
+import ShareIcon from "@material-ui/icons/Share";
 
 function VideoSidebar() {
   return (
     <div className="videoSidebar">
-      <h1>Ini Video Sidebar</h1>
+      <div className="videoSidebar__button">
+        <FavoriteIcon fontSize="large" />
+      </div>
+      <div className="videoSidebar__button">
+        <MessageIcon fontSize="large" />
+      </div>
+      <div className="videoSidebar__button">
+        <ShareIcon fontSize="large" />
+      </div>
     </div>
   );
 }
