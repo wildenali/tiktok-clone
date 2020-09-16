5af5ae0babad4d7cc4a41c7c99c51587fcffcaea
wildenali <wildenwildenaliali@gmail.com>
2020-09-16 14:31:25 +0700

    If the Fav Icon clicked, it will ber FavBorderIcon

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./VideoSidebar.css";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import MessageIcon from "@material-ui/icons/Message";
 import ShareIcon from "@material-ui/icons/Share";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 
 function VideoSidebar() {
+  const [liked, setLiked] = useState(false);
   return (
     <div className="videoSidebar">
       <div className="videoSidebar__button">
-        <FavoriteIcon fontSize="large" />
+        {liked ? (
+          <FavoriteIcon fontSize="large" />
+        ) : (
+          <FavoriteBorderIcon
+            fontSize="large"
+            onClick={(e) => setLiked(true)}
+          />
+        )}
+        <p>100</p>
       </div>
       <div className="videoSidebar__button">
         <MessageIcon fontSize="large" />
+        <p>250</p>
       </div>
       <div className="videoSidebar__button">
         <ShareIcon fontSize="large" />
+        <p>15</p>
       </div>
     </div>
   );
